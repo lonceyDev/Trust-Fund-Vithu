350685e6475868e0e69e55e59d5023f19c84aae7
Ambhika <lonceydev@gmail.com>
2023-12-13 13:41:30 +0530

    Install the Filament
    composer require filament/filament:"^3.1" -W
     php artisan filament:install --panels
    php artisan make:provider app/Providers/Filament/AdminPanelProvider.php

---
+++ app/Providers/app/Providers/Filament/AdminPanelProvider.php.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers\app\Providers\Filament;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminPanelProvider.php extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
