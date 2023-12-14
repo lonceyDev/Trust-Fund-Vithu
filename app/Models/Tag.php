341863fcb743a3773ae89c5e12a2d5a33a545e5c
Ambhika <lonceydev@gmail.com>
2023-12-14 08:57:10 +0530

    Model Relationship setting

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+    protected $guarded=[];
+
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+    }
+    public function events()
+    {
+        return $this->belongsToMany(Event::class);
+    }
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,20 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
