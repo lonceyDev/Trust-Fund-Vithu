209c9badec065e99346ee8eb03b8ebe9dabada65
Ambhika <lonceydev@gmail.com>
2023-12-14 08:57:10 +0530

    Model Relationship setting

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+    protected $guarded=[];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
 }
