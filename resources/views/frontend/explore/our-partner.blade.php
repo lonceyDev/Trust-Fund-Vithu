4d9c9dcc7a2d143cd259e769d598e11d88c4fc96
lonceyDev <lonceydev@gmail.com>
2024-02-18 01:01:57 +0530

    Our Partners tap remove content text

--- resources/views/frontend/explore/our-partner.blade.php
+++ resources/views/frontend/explore/our-partner.blade.php
@@ -37,43 +37,7 @@
         </div>
     </div>
 
-
-    <div class="about-area pt-100 pb-70">
-        <div class="container">
-            <div class="row">
-                <p>
-                    We will maintain 100% Transparency, deliver our projects at 0% Administration Cost
-                </p>
-                <p>
-                    <b>Transparency – 100%</b> <br>
-                    Every donation will be acknowledged on the Vithu Trust Fund website and will go directly to the
-                    Vithu Trust Fund bank account. Also online, will be a Vithu Trust Fund wish list identifying the most
-                    needy resources. As funds are collected, you can see which of these resources have been donated and see
-                    exactly where your money has gone.
-                </p>
-
-                <p>
-                    <b>
-                        Administration cost: 0%</b> <br>
-                    You might wonder how an organisation can run with no administration cost. The volunteers of Vithu Trust
-                    Fund use their own resources without any salary being drawn from the Vithu Trust Fund account.
-                    ‘Administration cost’ such as printing, website hosting and hiring of venues, for example, will be done
-                    through commercial sponsorship and Trustees’ contributions, making every penny you donate available for
-                    the cause. All unavoidable operational costs such as bank charges, fees for fundraising namely Just
-                    Giving web link, etc are paid for by contributions from Trustees. Thereby, we ensure that 100% of your
-                    donations will be applied to worthy causes.
-                </p>
-
-                <p>
-                    <b>Loyalty towards those we – AID</b> <br>
-                    All monies raised, whether it be fund raising, individual donations or lobbying other organisations,
-                    will be dedicated purely to help the people we AID.
-                </p>
-
-            </div>
-
-        </div>
-        <div class="benefit-area three pt-100 pb-70">
+    <div class="benefit-area three pt-100 pb-70">
             <div class="container">
                 <div class="section-title">
                     <h2>Our Partners</h2>
@@ -168,7 +132,6 @@
                     </div>
                 </div>
             </div>
-        </div>
     </div>
     
 
