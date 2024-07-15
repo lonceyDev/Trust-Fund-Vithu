21aed6f51446c275183e59b0e9c69c953cf21451
ambhika <lonceydev@gmail.com>
2024-07-15 13:39:22 +0530

    privacy policy layout changed

--- resources/views/frontend/privacy-policy.blade.php
+++ resources/views/frontend/privacy-policy.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
+@extends('layouts.front.index_blade')
+@section('content')
 
     <h1>Privacy Policy</h1>
     <p>Last updated: June 05, 2024</p>
@@ -295,11 +286,11 @@
             <p>By email: info@lonceytech.com</p>
         </li>
         <li>
-            <p>By visiting this page on our website: <a href="https://vithu.org/contacts"
-                    rel="external nofollow noopener" target="_blank">https://vithu.org/contacts</a></p>
+            <p>By visiting this page on our website: 
+                <a href="https://vithu.org/contacts" rel="external nofollow noopener" target="_blank"> 
+                    https://vithu.org/contacts 
+                </a>
+            </p>
         </li>
     </ul>
-
-</body>
-
-</html>
+@endsection
